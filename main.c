46e51c5bff278826ff3e71299bbf31aa2c453be9
SebbeKYH <90825487+SebbeKYH@users.noreply.github.com>
2022-10-25 23:24:14 +0200

    updated version

--- main.c
+++ main.c
@@ -1,53 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 
-// Defining the filename to be opened
-const char* filename = "currencies.txt";
+#define ARRAY_SIZE 100
+
+typedef struct currency_table
+    {
+    char *country;
+    int value;
+    }CT;
+    CT currency_arr[ARRAY_SIZE];
+
+void read_file(char *in_file){
+    int i=0;
+
+
+    FILE *file_pointer;
+    file_pointer = fopen(in_file,"r");
+
+
+    // Removing *c%d will give us correct spacing but not value...
+    while(fscanf(file_pointer, "%s,*c%d", &currency_arr[i].country, &currency_arr[i].value)!=EOF){
+    i++;
+    }
+    fclose(file_pointer);
+}
 
 int main()
 {
-    // Open the file and read it...
-    FILE *in_file = fopen(filename, "r");
+    char* in_file = "currency.txt";
+    read_file(in_file);
+    int i = 0;
 
-    // Error handling
-    if (!in_file)
-    {
-        perror("fopen");
-        return 0;
+
+    //size_t array_elements = SIZEOF(CT);
+
+    for(i; i< ARRAY_SIZE; i++){
+        printf("%s\t%d\n", &currency_arr[i].country, currency_arr[i].value);
+
     }
-
-    // Function for reading and searching file.
-    struct stat sb;
-    if (stat(filename, &sb) == -1)
-    {
-        perror("stat");
-        return 0;
-    }
-
-    //variabel för att scanna ord till file_contents
-    char *file_contents = malloc(sb.st_size);
-
-    // går igenom hela filen ord för ord
-    while (fscanf(in_file, "%[^-\n ] ", file_contents) != EOF) {
-
-     char *ptr;
-     long ret;
-
-     // hämtar str om det är en str, och siffra om det är en siffra
-     ret = strtol(file_contents, &ptr, 10);
-
-    // om ret är större än noll så printar vi siffran och ny rad
-      if (ret > 0)
-      {
-        printf("%s\n", file_contents);
-        continue;
-      }
-      printf("%s ", file_contents);
-      }
-
-    fclose(in_file);
-
     return 0;
 }
 
